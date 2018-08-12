f537a5b939b1c32c04e0ef1ce362e4626f634e10
b-vladimir <37156007+b-vladimir@users.noreply.github.com>
2018-08-12 11:24:53 +0300

    Done 2 task

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,5 +1,11 @@
 public class MyFirstProgram{
+
 	public static void main(String [] args){
-		System.out.println("Hello world!");
+		Point a = new Point(4, 2);
+		Point b = new Point(5, 6);
+
+		System.out.println("Distance between a point (" + a.x+ ", " + a.y + ")" +
+				" and point b (" + b.x+ ", " + b.y + ") " + "equals "+ a.distance(a, b));
 	}
+
 }
