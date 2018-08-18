b75fa8a19824480f252e0b736ec89a62ecbb4ef7
b-vladimir <37156007+b-vladimir@users.noreply.github.com>
2018-08-18 23:58:27 +0300

    Добавление кроссбраузерности в тесты

--- addressbook-web-tests/src/test/java/com/pjft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/com/pjft/addressbook/appmanager/ApplicationManager.java
@@ -2,7 +2,10 @@ package com.pjft.addressbook.appmanager;
 
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
@@ -14,9 +17,20 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
   private ContactHelper contactHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser.equals(BrowserType.CHROME)){
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.FIREFOX)){
+      wd = new FirefoxDriver();
+    }else if (browser.equals(BrowserType.IE)){
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     wd.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
     wait = new WebDriverWait(wd, 30);
