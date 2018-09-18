75e732621b328d96f6ace001ced822bc985909ce
b-vladimir <37156007+b-vladimir@users.noreply.github.com>
2018-09-18 18:47:24 +0300

    параметризация теста для создания группы(данные читаются из файла)

--- addressbook-web-tests/src/test/java/com/pjft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/com/pjft/addressbook/tests/GroupCreationTests.java
@@ -2,16 +2,34 @@ package com.pjft.addressbook.tests;
 
 import com.pjft.addressbook.model.GroupData;
 import com.pjft.addressbook.model.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase{
 
-  @Test
-  public void testGroupCreation() {
-    GroupData group = new GroupData().withName("test1").withHeader("test2").withFooter("test3");
+  @DataProvider
+  public Iterator<Object[]> validGroups() throws IOException {
+    List<Object[]> list = new ArrayList<>();
+    BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.csv"));
+    String line = reader.readLine();
+    while (line != null){
+      String[] split = line.split(";");
+      list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
     app.group().create(group);
