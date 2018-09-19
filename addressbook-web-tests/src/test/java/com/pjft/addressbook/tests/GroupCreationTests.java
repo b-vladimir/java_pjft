794991f182aa1036f2d0ad4893caa0ab50f3585a
b-vladimir <37156007+b-vladimir@users.noreply.github.com>
2018-09-19 18:54:39 +0300

    реализация автоматического закрытия райтеров и ридеров

--- addressbook-web-tests/src/test/java/com/pjft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/com/pjft/addressbook/tests/GroupCreationTests.java
@@ -36,16 +36,17 @@ public class GroupCreationTests extends TestBase{
 
   @DataProvider
   public Iterator<Object[]> validGroupsJSON() throws IOException {
-    BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.json"));
-    String json ="";
-    String line = reader.readLine();
-    while (line != null){
-      json += line;
-      line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.json"));){
+      String json ="";
+      String line = reader.readLine();
+      while (line != null){
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<GroupData> group = gson.fromJson(json, new TypeToken<List<GroupData>>() {}.getType());
+      return group.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
-    Gson gson = new Gson();
-    List<GroupData> group = gson.fromJson(json, new TypeToken<List<GroupData>>() {}.getType());
-    return group.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
   @Test(dataProvider = "validGroupsJSON")
--- addressbook-web-tests/src/test/java/com/pjft/addressbook/tests/GroupDeleteTests.java
+++ addressbook-web-tests/src/test/java/com/pjft/addressbook/tests/GroupDeleteTests.java
@@ -2,13 +2,8 @@ package com.pjft.addressbook.tests;
 
 import com.pjft.addressbook.model.GroupData;
 import com.pjft.addressbook.model.Groups;
-import org.hamcrest.CoreMatchers;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.List;
-import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
