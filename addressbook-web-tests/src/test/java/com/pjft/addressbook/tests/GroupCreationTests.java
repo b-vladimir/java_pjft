02de9c9e50673ee898857e93b3b2c2b4910ced8d
b-vladimir <37156007+b-vladimir@users.noreply.github.com>
2018-10-08 19:08:11 +0300

    так же модификация тестов для создания и удаления групп: получение списка групп из БД, а не из ГУИ

--- addressbook-web-tests/src/test/java/com/pjft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/com/pjft/addressbook/tests/GroupCreationTests.java
@@ -52,9 +52,9 @@ public class GroupCreationTests extends TestBase{
   @Test(dataProvider = "validGroupsJSON")
   public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     app.group().create(group);
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after.size(), equalTo(before.size() +1));
 
     assertThat(after, equalTo(before.withAdd(group.withId(after.stream().mapToInt(GroupData::getId).max().getAsInt()))));
--- addressbook-web-tests/src/test/java/com/pjft/addressbook/tests/GroupDeleteTests.java
+++ addressbook-web-tests/src/test/java/com/pjft/addressbook/tests/GroupDeleteTests.java
@@ -13,17 +13,17 @@ public class GroupDeleteTests extends TestBase{
   @BeforeMethod
   public void ensureGroupPresence(){
     app.goTo().groupPage();
-    if (app.group().all().size() == 0){
+    if (app.db().groups().size() == 0){
       app.group().create(new GroupData().withName("test1").withHeader("test2").withFooter("test3"));
     }
   }
 
   @Test
   public void testDeleteGroup(){
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     GroupData deletedGroup = before.iterator().next();
     app.group().delete(deletedGroup);
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after.size() + 1, equalTo(before.size()));
 
     assertThat(after, equalTo(before.withOut(deletedGroup)));
